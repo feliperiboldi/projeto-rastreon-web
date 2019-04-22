01c92290a2d7d5425358caaefeb4043dc18159a3
Felipe Riboldi <feliperiboldimarques@gmail.com>
2019-04-22 14:56:20 -0300

    Estrutura do Projeto

---
+++ dist/js/main.js
@@ -0,0 +1,11 @@
+if($(window).width() > 991) {
+	$('.carousel-image').height($(window).height() - 0);
+    $(window).resize(function () {
+        $('.carousel-image').height($(window).height() - 0);
+    });
+    } else {
+    $('.carousel-image').height($(window).height() - 0);
+    $(window).resize(function () {
+        $('.carousel-image').height($(window).height() - 0);
+    });
+}
