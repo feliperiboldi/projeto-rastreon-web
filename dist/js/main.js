7ff30bf5d8a0b3e1fb08ceee5cc953ae8a4c5fb0
Felipe Riboldi <feliperiboldimarques@gmail.com>
2019-04-30 14:23:21 -0300

    Conclusão da Estrutura do Site

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,11 +1,21 @@
 if($(window).width() > 991) {
-	$('.carousel-image').height($(window).height() - 0);
+	$('.carousel-image').height($(window).height() - 56);
     $(window).resize(function () {
-        $('.carousel-image').height($(window).height() - 0);
+        $('.carousel-image').height($(window).height() - 56);
     });
     } else {
-    $('.carousel-image').height($(window).height() - 0);
+    $('.carousel-image').height($(window).height() - 56);
     $(window).resize(function () {
-        $('.carousel-image').height($(window).height() - 0);
+        $('.carousel-image').height($(window).height() - 56);
     });
 }
+
+$(function() {
+    $(window).on("scroll", function() {
+        if($(window).scrollTop() > 100) {
+            $(".main-menu").addClass("fixed-top");
+        } else {
+            $(".main-menu").removeClass("fixed-top");
+        }
+    });
+});
