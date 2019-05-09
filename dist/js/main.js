cb88bf48cce6f470abea6bf93efedebf7a2e7ae4
Felipe Riboldi <feliperiboldimarques@gmail.com>
2019-05-09 13:25:21 -0300

    Ajustes no Rodapé e Menu

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,12 @@
+let $doc = $('html, body');
+$('.nav-link').click(function() {
+    $doc.animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
+});
+
+
 if($(window).width() > 991) {
 	$('.carousel-image').height($(window).height() - 56);
     $(window).resize(function () {
